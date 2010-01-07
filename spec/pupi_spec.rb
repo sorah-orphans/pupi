948d4a5bfda3789b125aa7e7b5789fb6a5182fc0
sora_harakami <sora134@gmail.com>
2010-01-07 09:54:13 +0900

    write spec skeleton

---
+++ spec/pupi_spec.rb
@@ -0,0 +1,52 @@
+require 'lib/pupi.rb'
+
+describe 'Pupi class' do
+  before do
+    Dir.mkdir("./spec_tmp")
+    Dir.mkdir("./spec_tmp/a")
+    Dir.mkdir("./spec_tmp/b")
+    Dir.mkdir("./spec_tmp/c")
+    @a = Pupi.create('./spec_tmp/a')
+    @b = Pupi.create('./spec_tmp/b')
+  end
+
+  it 'files and directories is correct' do
+  end
+
+  it 'create page and commit' do
+  end
+
+  it 'show page' do
+  end
+
+  it 'create another page and check rendered page' do
+  end
+
+  it 'push to another puki' do
+  end
+
+  it 'pull from another puki' do
+  end
+
+  it 'merge another puki changes' do
+  end
+
+  it 'clone another puki' do
+  end
+
+  after do
+    # thanks to http://www.namaraii.com/rubytips/?%A5%C7%A5%A3%A5%EC%A5%AF%A5%C8%A5%EA#l2
+
+    l = Dir::glob("./spec_tmp/**/").sort do |a,b|
+       b.split('/').size <=> a.split('/').size
+    end
+
+    l.each {|d|
+      Dir::foreach(d) do |f|
+        File::delete(d + f) unless /\.+$/ =~ f
+      end
+
+      Dir::rmdir(d)
+    }
+  end
+end
